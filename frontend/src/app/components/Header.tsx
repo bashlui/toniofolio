8a0eb3abf431db7e20efc00102d72cb1c46fba30
bashlui <luisboltech005@outlook.com>
2025-01-25 20:05:57 -0600

    Modified Hero, tuning...

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,15 +1,10 @@
 export const Header = () => {
   return (
     <header className="sticky top-0 backdrop-blur-sm z-20">
-      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
-        <p className="text-white/60 hidden md:block">
-          Productive. Creative. Passionate.
-        </p>
-      </div>
-      <div className="py-5">
+      <div className="py-5 bg-black">
         <div className="container">
           <div className="flex items-center justify-end">
-            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+            <nav className="hidden md:flex gap-6 text-white items-center">
               <a href="#" className="hover:text-black">
                 Home
               </a>
--- frontend/src/app/components/Hero.tsx
+++ frontend/src/app/components/Hero.tsx
@@ -1,17 +1,19 @@
 export const Hero = () => {
   return (
-    <section className="pt-8 pb-20 w-full h-full bg-black"> 
+    <section className="pt-8 pb-20 w-full h-full bg-black">
       <div className="container">
         <div className="flex justify-center">
-            <div className="w-auto">
-                <h1 className="text-6xl font-bold text-white">
-                Hello! <span className="text-yellow-600">Antonio, </span>here
-                </h1>
-                <p className="text-white/60 mt-3">
-                Find your dream job in the tech industry.
-                </p>
-                <button className="btn btn-purple mt-4 text-white">Get Started</button>
-            </div>
+          <div className="w-auto">
+            <h1 className="text-6xl font-bold text-white">
+              hello, <span className="bg-gradient-to-b from-indigo-600 to-cyan-400 text-transparent bg-clip-text">toño</span> here
+            </h1>
+            <p className="text-white/60 mt-3 text-center code-text">
+              c.s. student @ tec, full-stack dev in progress :)
+            </p>
+            <button className="btn btn-purple mt-4 text-white">
+              Get Started
+            </button>
+          </div>
         </div>
       </div>
     </section>
