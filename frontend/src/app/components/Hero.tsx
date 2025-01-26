a5aded621d82b62df6103f3684554bc0ed41a181
bashlui <luisboltech005@outlook.com>
2025-01-25 23:01:15 -0600

    Framer motion animations added

--- frontend/src/app/components/Hero.tsx
+++ frontend/src/app/components/Hero.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { AiOutlineGithub, AiFillLinkedin, AiOutlineFile } from "react-icons/ai";
+import { motion } from "framer-motion";
 
 export const Hero = () => {
   return (
-    <section className="pt-8 pb-20 w-full bg-black flex items-center justify-center">
+    <section className="pt-8 pb-20 w-full h-screen bg-black flex items-center justify-center">
       <div className="container">
         <div className="flex justify-center items-center">
           <div className="w-auto text-center">
@@ -20,9 +22,15 @@ export const Hero = () => {
               <AiOutlineGithub className="text-white/60 text-4xl mt-3" />
               <AiFillLinkedin className="text-white/60 text-4xl mt-3 ml-3" />
               <AiOutlineFile className="text-white/60 text-4xl mt-3 ml-3" />
-              <button className="btn bg-white mt-[6px] ml-6 hover:bg-white/80 transition duration-300 ease-in-out">
-                Convinced? Contact me!
-              </button>
+              <motion.button
+                whileHover={{
+                  scale: 1.1,
+                  boxShadow: "0px 0px 8px 2px rgba(255, 255, 255, 0.3)",
+                }}
+                className="btn bg-white mt-[6px] ml-6 hover:bg-gradient-to-b from-indigo-600 to-cyan-400 transition duration-300 ease-in-out"
+              >
+                Contact me!
+              </motion.button>
             </div>
           </div>
         </div>
